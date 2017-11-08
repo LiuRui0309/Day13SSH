2eba0fb3ccabf1976e6bef181ed1eb96e4acce9d
刘锐 <2941018235@qq.com>
2017-11-08 21:26:12 +0800

    添加UserAction的测试类

---
+++ src/test/java/com/lanou3g/ssh/action/UserActionTest.java
@@ -0,0 +1,16 @@
+package com.lanou3g.ssh.action;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dllo on 17/11/8.
+ */
+public class UserActionTest {
+
+    @Test
+    public void test(){
+
+    }
+}
